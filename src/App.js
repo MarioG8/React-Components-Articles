1e97f4cdb57ff55fe1b329c9c34fe49f74cc365b
MarioG8 <golikmariusz7@gmail.com>
2021-04-19 18:11:29 +0200

    Added article  cards with responsive grid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,78 @@ import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h3>Mariusz Golik Full-Stack JavaScript Developer.Hello World!:D :)!</h3>
-    </div>
+    <section className="article">
+      <div className="article__wrapper">
+        <div className="article__item">
+          <div className="article__img-container">
+            <img
+              className="article__img"
+              src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1366919.jpg&fm=jpg"
+              alt=""
+            />
+          </div>
+          <h2 className="article__header">Build & Test</h2>
+          <p className="article__desc">
+            Get work done quicker by building out entire projects or isolating
+            code to test features and animations. Want to keep it all under
+            wraps? Upgrade to a PRO account to keep your work private. wraps?
+            Upgrade to a PRO account to keep your work private.
+          </p>
+          <button className="article__btn">Try this</button>
+        </div>
+        <div className="article__item">
+          <div className="article__img-container">
+            <img
+              className="article__img"
+              src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1366919.jpg&fm=jpg"
+              alt=""
+            />
+          </div>
+          <h2 className="article__header">Build & Test</h2>
+          <p className="article__desc">
+            Get work done quicker by building out entire projects or isolating
+            code to test features and animations. Want to keep it all under
+            wraps? Upgrade to a PRO account to keep your work private. wraps?
+            Upgrade to a PRO account to keep your work private.
+          </p>
+          <button className="article__btn">Try this</button>
+        </div>
+        <div className="article__item">
+          <div className="article__img-container">
+            <img
+              className="article__img"
+              src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1366919.jpg&fm=jpg"
+              alt=""
+            />
+          </div>
+          <h2 className="article__header">Build & Test</h2>
+          <p className="article__desc">
+            Get work done quicker by building out entire projects or isolating
+            code to test features and animations. Want to keep it all under
+            wraps? Upgrade to a PRO account to keep your work private. wraps?
+            Upgrade to a PRO account to keep your work private.
+          </p>
+          <button className="article__btn">Try this</button>
+        </div>
+        <div className="article__item">
+          <div className="article__img-container">
+            <img
+              className="article__img"
+              src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1366919.jpg&fm=jpg"
+              alt=""
+            />
+          </div>
+          <h2 className="article__header">Build & Test</h2>
+          <p className="article__desc">
+            Get work done quicker by building out entire projects or isolating
+            code to test features and animations. Want to keep it all under
+            wraps? Upgrade to a PRO account to keep your work private. wraps?
+            Upgrade to a PRO account to keep your work private.
+          </p>
+          <button className="article__btn">Try this</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
