efadec4e5803114020a2e5e647b0ebf6cff189da
MarioG8 <golikmariusz7@gmail.com>
2021-04-21 18:37:59 +0200

    Added styles

--- src/App.js
+++ src/App.js
@@ -8,42 +8,22 @@ function App() {
         <Article
           img="https://images.pexels.com/photos/4220967/pexels-photo-4220967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
           title="Beauty of Mountains"
-          description="Get work done quicker by building out entire projects or isolating
-            code to test features and animations. Want to keep it all under
-            wraps? Upgrade to a PRO account to keep your work private. wraps?
-            Upgrade to a PRO account to keep your work private. wraps? Upgrade
-            to a PRO account to keep your work private. wraps? Upgrade to a PRO
-            account to keep your work private."
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores, ducimus beatae provident, cupiditate quas ipsam nihil illo veritatis laboriosam aut! Architecto error, quisquam vitae est perspiciatis fuga eaque ea dolores vel magni ex expedita omnis quod temporibus unde consequatur reiciendis eum molestiae ipsa. Totam et quo accusantium quis itaque vitae error, culpa quaerat a quidem, quae molestias quia repellat?"
         />
         <Article
           img="https://images.pexels.com/photos/1448055/pexels-photo-1448055.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
           title="Beauty of Mountains"
-          description="Get work done quicker by building out entire projects or isolating
-            code to test features and animations. Want to keep it all under
-            wraps? Upgrade to a PRO account to keep your work private. wraps?
-            Upgrade to a PRO account to keep your work private. wraps? Upgrade
-            to a PRO account to keep your work private. wraps? Upgrade to a PRO
-            account to keep your work private."
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores, ducimus beatae provident, cupiditate quas ipsam nihil illo veritatis laboriosam aut! Architecto error, quisquam vitae est perspiciatis fuga eaque ea dolores vel magni ex expedita omnis quod temporibus unde consequatur reiciendis eum molestiae ipsa. Totam et quo accusantium quis itaque vitae error, culpa quaerat a quidem, quae molestias quia repellat?"
         />
         <Article
           img="https://images.pexels.com/photos/947448/pexels-photo-947448.jpeg?cs=srgb&dl=pexels-tembela-bohle-947448.jpg&fm=jpg"
           title="Beauty of Mountains"
-          description="Get work done quicker by building out entire projects or isolating
-            code to test features and animations. Want to keep it all under
-            wraps? Upgrade to a PRO account to keep your work private. wraps?
-            Upgrade to a PRO account to keep your work private. wraps? Upgrade
-            to a PRO account to keep your work private. wraps? Upgrade to a PRO
-            account to keep your work private."
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores, ducimus beatae provident, cupiditate quas ipsam nihil illo veritatis laboriosam aut! Architecto error, quisquam vitae est perspiciatis fuga eaque ea dolores vel magni ex expedita omnis quod temporibus unde consequatur reiciendis eum molestiae ipsa. Totam et quo accusantium quis itaque vitae error, culpa quaerat a quidem, quae molestias quia repellat?"
         />
         <Article
           img="https://images.pexels.com/photos/2222839/pexels-photo-2222839.jpeg?cs=srgb&dl=pexels-valdemaras-d-2222839.jpg&fm=jpg"
           title="Beauty of Mountains"
-          description="Get work done quicker by building out entire projects or isolating
-            code to test features and animations. Want to keep it all under
-            wraps? Upgrade to a PRO account to keep your work private. wraps?
-            Upgrade to a PRO account to keep your work private. wraps? Upgrade
-            to a PRO account to keep your work private. wraps? Upgrade to a PRO
-            account to keep your work private."
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores, ducimus beatae provident, cupiditate quas ipsam nihil illo veritatis laboriosam aut! Architecto error, quisquam vitae est perspiciatis fuga eaque ea dolores vel magni ex expedita omnis quod temporibus unde consequatur reiciendis eum molestiae ipsa. Totam et quo accusantium quis itaque vitae error, culpa quaerat a quidem, quae molestias quia repellat?"
         />
       </div>
     </section>
